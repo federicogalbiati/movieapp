76ecf614f5572d9f99f10d1e5432bac5e40c8764
Fede <fedealbi04@gmail.com>
2024-04-12 22:11:15 +0200

    Aggiornamento html

--- js/dom_manipulator/createList.js
+++ js/dom_manipulator/createList.js
@@ -6,9 +6,9 @@
  * @returns
  */
 
-export const createMovieCards = async (movie) => {
-    const cardsContainer = document.getElementById("cards-container");
-
+export const createMovieCards = async (movie, containerId) => {
+    const cardsContainer = document.getElementById(containerId);
+    
     movie.results.forEach(Element => {
         const card = document.createElement("div");
         card.classList.add("card");
@@ -17,25 +17,32 @@ export const createMovieCards = async (movie) => {
         img.src = "https://image.tmdb.org/t/p/w500" + Element.poster_path;
         img.alt = movie.title;
 
-        const title = document.createElement("h2");
-        title.textContent = Element.title;
-
         const releaseDate = document.createElement("p");
-        releaseDate.textContent = "Data di uscita:" + Element.release_date;
+        releaseDate.textContent = "Data di uscita: " + Element.release_date;
 
         const vote = document.createElement("p");
-        vote.textContent = "Voto:" + Element.vote_average;
+        vote.textContent = "Voto: " + Element.vote_average;
 
         card.appendChild(img);
-        card.appendChild(title);
         card.appendChild(releaseDate);
         card.appendChild(vote);
 
         cardsContainer.appendChild(card);
     });
-}
+};
 
+const scrollbarWidthTrendings = document.getElementById('container-trendings').offsetWidth - document.getElementById('container-trendings').clientWidth;
+document.getElementById('scrollbar-1').style.width = scrollbarWidthTrendings + 'px';
 
+const scrollbarWidthMovies = document.getElementById('container-movies').offsetWidth - document.getElementById('container-movies').clientWidth;
+document.getElementById('scrollbar-2').style.width = scrollbarWidthMovies + 'px';
+
+const scrollbarWidthTvSeries = document.getElementById('container-tvseries').offsetWidth - document.getElementById('container-tvseries').clientWidth;
+document.getElementById('scrollbar-3').style.width = scrollbarWidthTvSeries + 'px';
+/*
+const scrollbarWidthPerson = document.getElementById('container-person').offsetWidth - document.getElementById('container-person').clientWidth;
+document.getElementById('scrollbar-4').style.width = scrollbarWidthPerson + 'px';
+*/
 /*
        Da aggiungere:
      * id
@@ -45,3 +52,8 @@ export const createMovieCards = async (movie) => {
      * media_type
      * release_date
 */
+
+/* const title = document.createElement("h2");
+        title.textContent = Element.title;
+*/
+//card.appendChild(title);
